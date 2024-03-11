bdfc4907dda002b94063d833b51b2645c1c9282e
turtton <top.gear7509@turtton.net>
2024-03-11 18:37:38 +0900

    Add zsh configs
    Signed-off-by: turtton <top.gear7509@turtton.net>

--- home-manager/cli/shell/zsh/default.nix
+++ home-manager/cli/shell/zsh/default.nix
@@ -1,4 +1,4 @@
-{
+{pkgs, ...}: {
   imports = [./starship.nix];
   programs.zsh = {
     enable = true;
@@ -7,5 +7,35 @@
     enableCompletion = true;
     enableAutosuggestions = true;
     syntaxHighlighting.enable = true;
+
+    plugins = [
+      {
+        name = "zsh-nix-shell";
+        file = "nix-shell.plugin.zsh";
+        src = pkgs.fetchFromGitHub {
+          owner = "chisui";
+          repo = "zsh-nix-shell";
+          rev = "v0.8.0";
+          sha256 = "1lzrn0n4fxfcgg65v0qhnj7wnybybqzs4adz7xsrkgmcsr0ii8b7";
+        };
+      }
+      # {
+      #   name = "xiong-chiamiov-plus";
+      #   file = "themes/xiong-chiamiov-plus.zsh-theme";
+      #   src = pkgs.fetchgit {
+      #     url = "https://github.com/ohmyzsh/ohmyzsh.git";
+      #     rev = "4fd2af0a82e2826317d9551ecd8d5f44553828d7";
+      #     sparseCheckout = [
+      #       "themes"
+      #     ];
+      #     hash = "sha256-jCXBz5FAhGLusWBA9JMGcQ7mxDhOYC5PXyWHoV4OYz0=";
+      #   };
+      # }
+    ];
+    oh-my-zsh = {
+      enable = true;
+      plugins = [];
+      theme = "xiong-chiamiov-plus";
+    };
   };
 }
