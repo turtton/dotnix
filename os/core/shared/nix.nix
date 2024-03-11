767d1de07ff5c7fb6a98eb1f6b945b3a1f3cb88a
turtton <top.gear7509@turtton.net>
2024-03-11 15:48:45 +0900

    Add caches
    Signed-off-by: turtton <top.gear7509@turtton.net>

--- os/core/shared/nix.nix
+++ os/core/shared/nix.nix
@@ -5,6 +5,14 @@
       experimental-features = ["nix-command" "flakes"];
       trusted-users = ["root" "@wheel"];
       accept-flake-config = true;
+      substituters = [
+        "https://cache.nixos.org"
+        "https://nix-community.cachix.org"
+      ];
+      trusted-public-keys = [
+        "cache.nixos.org-1:6NCHdD59X431o0gWypbMrAURkbJ16ZPMQFGspcDShjY="
+        "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
+      ];
     };
     gc = {
       automatic = true;
