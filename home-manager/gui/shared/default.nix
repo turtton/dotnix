ec1f7273329053cded6a02fb98c3f5e3bd3bf399
turtton <top.gear7509@turtton.net>
2024-03-12 23:40:57 +0900

    Add fcitx env settings
    Signed-off-by: turtton <top.gear7509@turtton.net>

--- home-manager/gui/shared/default.nix
+++ home-manager/gui/shared/default.nix
@@ -1,7 +1,8 @@
 {
   imports = [
-    ./chat.nix
     ./browser.nix
+    ./chat.nix
+    ./fcitx.nix
     ./idea.nix
     ./kdeconnect.nix
     ./media.nix
---
+++ home-manager/gui/shared/fcitx.nix
@@ -0,0 +1,7 @@
+{
+  home.file.".xprofile".text = ''
+  export GTK_IM_MODULE=fcitx
+  export QT_IM_MODULE=fcitx
+  export XMODIFIERS=@im=fcitx
+  '';
+}
