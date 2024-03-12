10aa4085b2b4cb4f3fd36c0d9e90036790c3c240
turtton <top.gear7509@turtton.net>
2024-03-12 16:00:43 +0900

    WIP: Apply patch in idea

--- home-manager/gui/shared/idea.nix
+++ home-manager/gui/shared/idea.nix
@@ -1,19 +1,28 @@
 {pkgs, ...}: let 
-  ides = with pkgs.jetbrains; [
-    idea-ultimate
-    webstorm
-    rust-rover
-  ];
   # https://github.com/NixOS/nixpkgs/blob/nixos-unstable/pkgs/applications/editors/jetbrains/plugins/plugins.json
   plugins = [
     "ideavim"
     "nixidea"
     "github-copilot"
   ];
+  patched-idea = with pkgs; (jetbrains.plugins.addPlugins jetbrains.idea-ultimate plugins);
+  # Override LD_LIBREARY_PATH for minecraft mod dev(requires udev and libGL)
+  # See: LD libraries (os/core/shared/ld.nix)
+  # Script Reference: https://github.com/Mic92/nix-ld?tab=readme-ov-file#my-pythonnodejsrubyinterpreter-libraries-do-not-find-the-libraries-configured-by-nix-ld
+  idea = (pkgs.writeShellScriptBin "idea-ultimate" ''
+  export LD_LIBRARY_PATH=$NIX_LD_LIBRARY_PATH
+  exec ${patched-idea}/bin/idea-ultimate "$@"
+  '');
+  ides = with pkgs.jetbrains; [
+    webstorm
+    rust-rover
+  ];
 in  {
   home.packages = with pkgs; [
     android-studio
     # basically should not use toolbox because of issues(https://github.com/NixOS/nixpkgs/issues/240444) but useful to preview IDE 
     jetbrains-toolbox
-  ] ++ (map (ide: (jetbrains.plugins.addPlugins ide plugins)) ides);
+  ] 
+  ++ (map (ide: (jetbrains.plugins.addPlugins ide plugins)) ides) 
+  ++ [idea];
 }
