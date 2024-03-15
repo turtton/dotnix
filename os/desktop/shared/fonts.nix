12ea3d951b4cffd634b05f5abbe229c67b2c63de
turtton <top.gear7509@turtton.net>
2024-03-15 21:25:10 +0900

    Add twemoji

--- os/desktop/shared/fonts.nix
+++ os/desktop/shared/fonts.nix
@@ -6,23 +6,30 @@
       noto-fonts-cjk-sans
       noto-fonts-emoji
       nerdfonts
+      twemoji-color-font
     ];
     fontDir.enable = true;
     fontconfig = {
       defaultFonts = {
         serif = [
           "Noto Serif CJK JP"
+          "Twemoji"
           "Noto Color Emoji"
         ];
         sansSerif = [
           "Noto Sans CJK JP"
+          "Twemoji"
           "Noto Color Emoji"
         ];
         monospace = [
           "JetBrainsMono Nerd Font"
+          "Twemoji"
           "Noto Color Emoji"
         ];
-        emoji = ["Noto Color Emoji"];
+        emoji = [
+          "Twemoji"
+          "Noto Color Emoji"
+        ];
       };
     };
   };
