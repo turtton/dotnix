31be1b0372b5a08c7128415550817cdada727ed4
turtton <top.gear7509@turtton.net>
2024-03-16 12:06:54 +0900

    Remove invalid settings

--- os/desktop/1password.nix
+++ os/desktop/1password.nix
@@ -1,16 +1,11 @@
-{pkgs, ...}:
+{pkgs, username, ...}:
 {
   programs = {
     _1password.enable = true;
     _1password-gui = {
       enable = true;
-      polkitPolicyOwners = [ "turtton" ];
+      polkitPolicyOwners = [ username ];
     };
-    ssh.extraConfig = ''
-      Host *
-        IdentitiesOnly=yes
-        IdentityAgent ~/.1password/agent.sock
-    '';
   };
   environment.etc = {
     "1password/custom_allowed_browsers" = {
