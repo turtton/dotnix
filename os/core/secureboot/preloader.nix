e8eff0f768b390ee230aeb559143bcc4858920b9
turtton <top.gear7509@turtton.net>
2024-03-16 23:11:05 +0900

    Add documents about secureboot related error

--- os/core/secureboot/preloader.nix
+++ os/core/secureboot/preloader.nix
@@ -1,3 +1,6 @@
+# This nix file is for installing PreLoader and HashTool to systemd-boot.
+# After enabling this configuration, you need to add `--install-bootloader` option in nixos-install command to resolve builder error.
+# Error line : https://github.com/NixOS/nixpkgs/blob/3b48b3aaa9f53eefd61c334ece1a08b20f1a2e0c/nixos/modules/system/boot/loader/systemd-boot/systemd-boot-builder.py#L304
 efiSystemDrive: efiPartId: { pkgs, ...} : let 
   # Original: https://aur.archlinux.org/packages/preloader-signed
   # See: https://wiki.archlinux.jp/index.php/Unified_Extensible_Firmware_Interface/%E3%82%BB%E3%82%AD%E3%83%A5%E3%82%A2%E3%83%96%E3%83%BC%E3%83%88#PreLoader
@@ -27,10 +30,16 @@ in {
     extraFiles = {
       "EFI/systemd/PreLoader.efi" = "${preLoader}/share/${preLoader.pname}.efi";
       "EFI/systemd/HashTool.efi" = "${hashTool}/share/${hashTool.pname}.efi";
+      # Fallback settings
+      "EFI/BOOT/HashTool.efi" = "${hashTool}/share/${hashTool.pname}.efi";
+      "EFI/BOOT/BOOTx64.EFI" = "${preLoader}/share/${preLoader.pname}.efi";
     };
     extraInstallCommands = ''
       # I dont know why, but this shell cannnot use cat command.
       ${pkgs.uutils-coreutils-noprefix}/bin/cp /boot/EFI/systemd/systemd-bootx64.efi /boot/EFI/systemd/loader.efi
+      # Fallback settings
+      ${pkgs.uutils-coreutils-noprefix}/bin/cp /boot/EFI/systemd/systemd-bootx64.efi /boot/EFI/BOOT/loader.efi
+      # ${pkgs.uutils-coreutils-noprefix}/bin/cp ${preLoader}/share/${preLoader.pname}.efi /boot/EFI/BOOT/BOOTX64.EFI
     '';
   };
   system.activationScripts = {
