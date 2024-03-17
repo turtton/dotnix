95e6671786c173fde3fb37a87b81b119c6f4df07
turtton <top.gear7509@turtton.net>
2024-03-17 18:59:13 +0900

    Add bockbench

--- home-manager/gui/shared/dev.nix
+++ home-manager/gui/shared/dev.nix
@@ -1,8 +1,7 @@
 { pkgs, ... }: {
   home.packages = with pkgs; [
     unityhub
-    # Wait until this pr merged https://github.com/NixOS/nixpkgs/pull/279795
-    # blockbench-electron
+    blockbench-electron
     postman
   ];
 }
