ca5628eaf4ddf2bd8401f1ddf29df35d9a1bb13d
turtton <top.gear7509@turtton.net>
2024-03-20 12:26:22 +0900

    Open port for kdeconnect

--- os/core/shared/network.nix
+++ os/core/shared/network.nix
@@ -6,6 +6,12 @@
     networkmanager.enable = true;
     firewall = {
       enable = true;
+      allowedTCPPortRanges = [
+        { from = 1714; to = 1764; } # KDE Connect
+      ];
+      allowedUDPPortRanges = [
+        { from = 1714; to = 1764; } # KDE Connect
+      ];
       # trustedInterfaces = ["tailscale0"];
       # allowedUDPPorts = [config.services.tailscale.port];
     };
