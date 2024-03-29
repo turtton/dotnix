609af669db6956ac2b4b850427b434e3908e8c07
turtton <top.gear7509@turtton.net>
2024-03-29 17:45:08 +0900

    Disable mise all_compile

--- home-manager/cli/shared/mise.nix
+++ home-manager/cli/shared/mise.nix
@@ -2,5 +2,10 @@
   programs.mise = {
     enable = true;
     enableZshIntegration = true;
+    settings = {
+      all_compile = false;
+      node_compile = false;
+      python_compile = false;
+    };
   };
 }
