b25aa6a1b7332bb0958644f433283660c82019b1
turtton <top.gear7509@turtton.net>
2024-03-29 18:38:23 +0900

    Revert settings and add asdf compat

--- home-manager/cli/shared/mise.nix
+++ home-manager/cli/shared/mise.nix
@@ -1,11 +1,11 @@
-{
+{ lib, ... }: {
   programs.mise = {
     enable = true;
     enableZshIntegration = true;
-    settings = {
-      all_compile = false;
-      node_compile = false;
-      python_compile = false;
-    };
+  };
+  home.activation = {
+    linkMiseDirToAsdf = lib.hm.dag.entryAfter [ "writeBoundary" ] ''
+      run ln -s $VERBOSE_ARG $HOME/.local/share/mise $HOME/.asdf
+    '';
   };
 }
