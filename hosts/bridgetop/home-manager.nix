9541ed5c52563a583acf3df698105bc08cad0c1b
turtton <top.gear7509@turtton.net>
2024-04-11 00:01:57 +0900

    Load plasma settings

--- hosts/bridgetop/home-manager.nix
+++ hosts/bridgetop/home-manager.nix
@@ -1,5 +1,7 @@
 {
   imports = [
+    ./plasma/plasma.nix
+    ./plasma/plasma_generated.nix
     ./../../home-manager/cli/shared
     (import ./../../home-manager/cli/git.nix { userName = "turtton"; userEmail = "top.gear7509@turtton.net"; signingKey = "8152FC5D0B5A76E1"; })
     ./../../home-manager/cli/shell/zsh
