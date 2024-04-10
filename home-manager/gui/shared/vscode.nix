82b0c7d5ea391a429eb514d6920876f583d449ef
turtton <top.gear7509@turtton.net>
2024-04-10 22:31:00 +0900

    Change plugin target because of download error

--- home-manager/gui/shared/vscode.nix
+++ home-manager/gui/shared/vscode.nix
@@ -12,7 +12,7 @@ in
       vscode-marketplace.github.copilot-chat
       vscode-marketplace.github.copilot
       open-vsx.pinage404.nix-extension-pack
-      open-vsx."42crunch".vscode-openapi
+      vscode-marketplace."42crunch".vscode-openapi
       # https://github.com/nix-community/nix-vscode-extensions/issues/31
       (vscode-marketplace.typespec.typespec-vscode.overrideAttrs (_: { sourceRoot = "extension"; }))
       open-vsx.asvetliakov.vscode-neovim
