24a4f9adbaf6e0e7fece7b7bb9e861125ec5cd25
turtton <top.gear7509@turtton.net>
2024-05-08 21:07:00 +0900

    Fix missed semicolon

--- hosts/atticserver/nixos.nix
+++ hosts/atticserver/nixos.nix
@@ -11,27 +11,27 @@
     ./atticd.nix
     ./../../os/core/shared
     ./../../os/core/shell.nix
-  ]
-    # ++ (with inputs.nixos-hardware.nixosModules; [
-    # common-cpu-amd
-    # common-pc-ssd
-    # ]);
+  ];
+  # ++ (with inputs.nixos-hardware.nixosModules; [
+  # common-cpu-amd
+  # common-pc-ssd
+  # ]);
 
-    boot = {
-  loader = {
-  systemd-boot.enable = true;
-  efi.canTouchEfiVariables = true;
-};
-kernelPackages = pkgs.linuxKernel.packages.linux_xanmod;
-};
+  boot = {
+    loader = {
+      systemd-boot.enable = true;
+      efi.canTouchEfiVariables = true;
+    };
+    kernelPackages = pkgs.linuxKernel.packages.linux_xanmod;
+  };
 
-system.stateVersion = "24.04";
+  system.stateVersion = "24.04";
 
-# Define a user account. Don't forget to set a password with ‘passwd’.
-users.users."${username}" = {
-isNormalUser = true;
-description = "${username}";
-extraGroups = [ "networkmanager" "wheel" ];
-shell = pkgs.zsh;
-};
+  # Define a user account. Don't forget to set a password with ‘passwd’.
+  users.users."${username}" = {
+    isNormalUser = true;
+    description = "${username}";
+    extraGroups = [ "networkmanager" "wheel" ];
+    shell = pkgs.zsh;
+  };
 }
