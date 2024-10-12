3678c0fed1348556dbfb8b5fa48bfee18dfb3f64
turtton <top.gear7509@turtton.net>
2024-10-12 23:52:33 +0900

    Format codes

--- home-manager/cli/shared/nvim/default.nix
+++ home-manager/cli/shared/nvim/default.nix
@@ -1,17 +1,17 @@
 { pkgs, lib, inputs, ... }:
 let
-	nvim-project = inputs.turtton-neovim.packages.${pkgs.system};
+  nvim-project = inputs.turtton-neovim.packages.${pkgs.system};
 in
 {
   programs.neovim = {
     vimAlias = true;
-		defaultEditor = true;
+    defaultEditor = true;
   };
-	home.packages = [
-		nvim-project.default
-	];
-	home.file.".config/nvim" = {
-		source = nvim-project.config;
-		recursive = true;
-	};
+  home.packages = [
+    nvim-project.default
+  ];
+  home.file.".config/nvim" = {
+    source = nvim-project.config;
+    recursive = true;
+  };
 }
