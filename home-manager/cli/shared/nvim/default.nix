7fa996efe133c0415f6d21dd5db9689f75ddf4d3
turtton <top.gear7509@turtton.net>
2024-11-03 18:56:45 +0900

    🎨 Formart codes

--- home-manager/cli/shared/nvim/default.nix
+++ home-manager/cli/shared/nvim/default.nix
@@ -3,12 +3,12 @@ let
   nvim-project = inputs.turtton-neovim.packages.${pkgs.system};
 in
 {
-	home = {
-		packages = [ nvim-project.default ];
-		sessionVariables.EDITOR = "nvim";
-		file.".config/nvim" = {
-		  source = nvim-project.config;
-		  recursive = true;
-		};
-	};
+  home = {
+    packages = [ nvim-project.default ];
+    sessionVariables.EDITOR = "nvim";
+    file.".config/nvim" = {
+      source = nvim-project.config;
+      recursive = true;
+    };
+  };
 }
