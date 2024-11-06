20f8a0e2357ab07347e366d7e578c8750002c7ce
turtton <top.gear7509@turtton.net>
2024-11-06 20:33:13 +0900

    🎨 Format codes

--- home-manager/wm/hyprland/rofi/default.nix
+++ home-manager/wm/hyprland/rofi/default.nix
@@ -1,7 +1,7 @@
 { pkgs, ... }: {
   programs.rofi = {
     enable = true;
-    package = pkgs.rofi-wayland.overrideAttrs(old: { plugins = [ pkgs.rofi-emoji ]; });
+    package = pkgs.rofi-wayland.overrideAttrs (old: { plugins = [ pkgs.rofi-emoji ]; });
     #	Refered: https://github.com/NeshHari/XMonad/blob/main/rofi/.config/rofi/config.rasi
     theme = ./rofi.rasi;
   };
