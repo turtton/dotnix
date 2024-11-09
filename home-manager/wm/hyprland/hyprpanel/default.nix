b0586a136bf4228ea27481b55e3110fc2801376e
turtton <top.gear7509@turtton.net>
2024-11-09 14:04:30 +0900

    ➕ Add hyprpanel required apps

--- home-manager/wm/hyprland/hyprpanel/default.nix
+++ home-manager/wm/hyprland/hyprpanel/default.nix
@@ -1,9 +1,16 @@
 { pkgs, ... }: {
   home.packages = with pkgs; [
     hyprpanel # status bar and notification daemon
+    networkmanager
+    gnome-bluetooth
+    libgtop
+    bluez
+    bluez-tools
+    brightnessctl
     gpu-screen-recorder
     hyprpicker
     grimblast
+    btop
   ];
   home.file.".cache/ags/hyprpanel/options.json".source = ./hyprpanel_config.json;
 }
