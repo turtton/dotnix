4ddfff98484762d9640a27363ee6b4d92af94371
turtton <top.gear7509@turtton.net>
2024-11-20 18:07:29 +0900

    🚸 Specify workspace on first launch discord/vivaldi

--- hosts/maindesk/home-manager-hypr.nix
+++ hosts/maindesk/home-manager-hypr.nix
@@ -28,13 +28,10 @@
     };
     exec-once = [
       "1password"
-      "discord"
+      "[workspace 1 silent] discord"
+      "[workspace 6 silent] vivaldi"
       "steam -silent"
       "KEYBASE_AUTOSTART=1 keybase-gui"
-    ];
-    windowrulev2 = [
-      "workspace 1 fullscreen 1 opaque, class:^(discord)$"
-      "workspace 6 opaque, class:^(vivaldi)(.*)$"
     ];
   };
 }
