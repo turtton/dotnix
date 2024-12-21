ea526e57a177e45d87581d8bd36e7559dfd2772d
turtton <top.gear7509@turtton.net>
2024-12-21 22:34:12 +0900

    🚸 Set screen off before lock

--- home-manager/wm/hyprland/hypridle.nix
+++ home-manager/wm/hyprland/hypridle.nix
@@ -11,12 +11,12 @@
       listener = [
         {
           timeout = 900;
-          on-timeout = "hyprlock";
-        }
-        {
-          timeout = 1200;
           on-timeout = "hyprctl dispatch dpms off";
           on-resume = "hyprctl dispatch dpms on";
+        }
+        {
+          timeout = 910;
+          on-timeout = "hyprlock";
         }
         # {
         #   timeout = 1800;
