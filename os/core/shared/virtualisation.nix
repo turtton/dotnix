79f9e6be8a76a83afee01eb830af66e107f032f0
turtton <top.gear7509@turtton.net>
2024-12-30 15:28:54 +0900

    ✨ Improve qemu tpm support

--- os/core/shared/virtualisation.nix
+++ os/core/shared/virtualisation.nix
@@ -8,7 +8,23 @@
       };
     };
     podman.enable = true;
-    libvirtd.enable = true;
+    libvirtd = {
+      enable = true;
+      qemu = {
+        package = pkgs.qemu_kvm;
+        runAsRoot = true;
+        swtpm.enable = true;
+        ovmf = {
+          enable = true;
+          packages = [
+            (pkgs.OVMFFull.override {
+              secureBoot = true;
+              tpmSupport = true;
+            }).fd
+          ];
+        };
+      };
+    };
   };
   environment.systemPackages = with pkgs; [
     virt-manager
