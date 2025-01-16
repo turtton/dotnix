fa76f02bba77bafaab77153d4e603beb00a72c63
turtton <top.gear7509@turtton.net>
2025-01-16 17:45:08 +0900

    🔥 Remove linux depended codes

--- home-manager/gui/dev/idea/default.nix
+++ home-manager/gui/dev/idea/default.nix
@@ -4,44 +4,10 @@ let
   plugins = [
     "ideavim"
     "nixidea"
-    "github-copilot"
-  ];
+  ] ++ pkgs.lib.optionals pkgs.hostPlatform.isLinux [ "github-copilot" ];
   applyPlugins = ide: with pkgs; (jetbrains.plugins.addPlugins ide plugins);
-  patched-idea = with pkgs; (applyPlugins jetbrains.idea-ultimate);
-  requiredLibPath = with pkgs; lib.makeLibraryPath [
-    libGL
-    udev
-  ];
-  # Override LD_LIBREARY_PATH for minecraft mod dev(requires udev and libGL)
-  # https://github.com/Gerschtli/nix-config/blob/763a8f515701ccab125174d241c2331fb72071e3/home/programs/idea-ultimate.nix#L66
-  # https://github.com/Gerschtli/nix-config/blob/763a8f515701ccab125174d241c2331fb72071e3/lib/wrap-program.nix#L4
-  idea = pkgs.symlinkJoin {
-    name = "idea-ultimate-wrapped";
-    paths = [ patched-idea ];
-    buildInputs = [ pkgs.makeWrapper ];
-    postBuild =
-      let
-        desktopEntryPath = "/share/applications/idea-ultimate.desktop";
-        path = "/bin/idea-ultimate";
-      in
-      ''
-        # desktop entry
-        if [[ -L "$out/share/applications" ]]; then
-          rm "$out/share/applications"
-          mkdir "$out/share/applications"
-        else
-          rm "$out${desktopEntryPath}"
-        fi
-
-        sed -e "s|Exec=${patched-idea + path}|Exec=$out${path}|" \
-          "${patched-idea + desktopEntryPath}" \
-          > "$out${desktopEntryPath}"
-
-        wrapProgram "$out${path}" \
-          --prefix LD_LIBRARY_PATH : ${requiredLibPath}
-      '';
-  };
   ides = with pkgs.jetbrains; [
+    idea-ultimate
     webstorm
     rust-rover
     datagrip
@@ -50,12 +16,11 @@ let
   ];
 in
 {
-  home.packages = with pkgs; [
+  home.packages = with pkgs; lib.optionals hostPlatform.isLinux [
+    android-studio
     # basically should not use toolbox because of issues(https://github.com/NixOS/nixpkgs/issues/240444) but useful to preview IDE 
     jetbrains-toolbox
   ]
-  ++ lib.optionals hostPlatform.isLinux [ android-studio ]
-  ++ (map (ide: (applyPlugins ide)) ides)
-  ++ [ idea ];
+  ++ (map (ide: (applyPlugins ide)) ides);
   home.file.".ideavimrc".source = ./ideavimrc;
 }
