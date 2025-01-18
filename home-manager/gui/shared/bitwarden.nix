51c2971997bb4a44963040d03df97aa98edecfca
turtton <top.gear7509@turtton.net>
2025-01-18 14:56:05 +0900

    🔧 Update bitwarden related settings

--- home-manager/gui/shared/bitwarden.nix
+++ home-manager/gui/shared/bitwarden.nix
@@ -1,9 +1,16 @@
+# See also os/desktop/shared/bitwarden.nix
 { pkgs, hostPlatform, ... }: {
   programs.rbw = {
     enable = true;
-    email = "fun.dust0146@turtton.net";
+    settings = {
+      email = "fun.dust0146@turtton.net";
+      pinentry = if hostPlatform.isLinux then pkgs.pinentry-qt else pkgs.pinentry_mac;
+    };
   };
-  home.packages = with pkgs; lib.options hostPlatform.isLinux [
+  home.packages = with pkgs; [
+    bitwarden-cli
+  ] ++ lib.optionals hostPlatform.isLinux [
     bitwarden-desktop
+    keyguard
   ];
 }
