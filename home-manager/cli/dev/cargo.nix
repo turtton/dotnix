4b3b69810d954482a7837a07cf6ee1ab319115fc
turtton <top.gear7509@turtton.net>
2025-03-02 22:34:26 +0900

    ➕ Add sccache

--- home-manager/cli/dev/cargo.nix
+++ home-manager/cli/dev/cargo.nix
@@ -9,13 +9,19 @@
     cargo-features-manager # unused feature detector
     cargo-watch # auto-reload
     # cargo-vet # crate security checker TODO: https://github.com/NixOS/nixpkgs/pull/370510
+    sccache # ccache like compiler cache
     crate2nix
     rustowl
   ];
   # clang+mold could not resolve devEnv libraries defined in flake
+  # so we need to set env vars in each project with `mold.override(p: { extraPackages = [ openssl ]; })`
   # home.file.".cargo/config.toml".text = ''
   #   		[target.x86_64-unknown-linux-gnu]
   #   		linker = "${pkgs.clang}/bin/clang"
   #   		rustflags = ["-C", "link-arg=-fuse-ld=${pkgs.mold}/bin/mold"]
   #   		'';
+  home.file.".cargo/config.toml".text = ''
+    	[build]
+    	rustc-wrapper = "${pkgs.sccache}/bin/sccache"
+    	'';
 }
