c383031e9ede26766c9a4d0a0d8b4d9fc79fab5a
turtton <top.gear7509@turtton.net>
2025-03-16 16:13:46 +0900

    🔧 Add git configurations

--- home-manager/cli/git.nix
+++ home-manager/cli/git.nix
@@ -16,7 +16,11 @@
         line-numbers = true;
       };
     };
+    # Some ocnfigurations reffered from https://blog.gitbutler.com/how-git-core-devs-configure-git/
     extraConfig = {
+      column.ui = "auto";
+      branch.sort = "-committerdate";
+      tag.sort = "version:refname";
       init.defaultBranch = "main";
       core = {
         autocrlf = "input";
@@ -25,6 +29,27 @@
       pack = {
         windowMemory = "2g";
         packSizeLimit = "1g";
+      };
+      push = {
+        default = "simple";
+        autoSetupRemote = true;
+        followTags = true;
+      };
+      fetch = {
+        prune = true;
+        pruneTags = true;
+        all = true;
+      };
+      help.autocorrect = "prompt";
+      commit.verbose = true;
+      rerere = {
+        enabled = true;
+        autoupdate = true;
+      };
+      rebase = {
+        autoSquash = true;
+        autoStash = true;
+        updateRefs = true;
       };
     };
     ignores = [ ".idea" ".vscode" ".memo.local.md" ];
