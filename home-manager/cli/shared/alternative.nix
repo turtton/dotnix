394afea0dac9a190a670b293314cce4b28c02ec5
turtton <top.gear7509@turtton.net>
2025-03-16 11:43:36 +0900

    🔧 Improve atuin/zoxide settings

--- home-manager/cli/shared/alternative.nix
+++ home-manager/cli/shared/alternative.nix
@@ -7,8 +7,20 @@
     fd # find 
     ripgrep # grep
     delta # diff
-    zoxide # cd
   ];
-  # History Search(ctrl+r in zsh)
-  programs.atuin.enable = true;
+  programs = {
+    # cd
+    zoxide.enable = true;
+    # History Search(ctrl+r in zsh)
+    atuin = {
+      enable = true;
+      flags = [ "--disable-up-arrow" ];
+    };
+    # Improve up-arrow search behavior
+    zsh.initExtra = ''
+      autoload -Uz history-beginning-search-backward
+      zle -N history-beginning-search-backward
+      bindkey '^[[A' history-beginning-search-backward
+    '';
+  };
 }
