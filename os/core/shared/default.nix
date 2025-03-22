994c02789e163ad74ad57f6c10e871a27124dcd7
turtton <top.gear7509@turtton.net>
2025-03-22 18:23:55 +0900

    🔧 Add boot configuration limit

--- os/core/shared/default.nix
+++ os/core/shared/default.nix
@@ -18,4 +18,11 @@
   # Enable usb access
   services.gvfs.enable = true;
   services.udisks2.enable = true;
+
+  # Limit the number of boot loader configurations
+  boot.loader = {
+    systemd-boot.configurationLimit = 5;
+    grub.configurationLimit = 5;
+    generic-extlinux-compatible.configurationLimit = 5;
+  };
 }
