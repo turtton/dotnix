78193c02387a6644fbd2ac7abe0b1fc1fa53205b
turtton <top.gear7509@turtton.net>
2025-03-27 11:11:38 +0900

    🐛 Add patch to build beutl

--- overlay/beutl/default.nix
+++ overlay/beutl/default.nix
@@ -5,23 +5,26 @@ gen: self: prev:
     pname = "Beutl";
     version = lib.removePrefix "v" gen.version;
 
-    projectFile = "Beutl.slnx";
-    nugetDeps = ./beutl.json;
+    projectFile = "src/Beutl/Beutl.csproj";
+    nugetDeps = ./deps.json;
     mapNuGetDependencies = true;
-    #useAppHost = false;
     # Sometimes fails with "The process cannot access the file because it is being used by another process."
     enableParallelBuilding = false;
 
     buildInputs = [ ffmpeg ];
-    runtimeDeps = [ ffmpeg mono fontconfig ];
+    runtimeDeps = [ ffmpeg fontconfig ];
 
     dotnet-sdk = dotnetCorePackages.sdk_9_0-bin;
     dotnet-runtime = dotnetCorePackages.runtime_9_0-bin;
 
+    selfContained = true;
     dotnetInstallFlags = [
-      "-p:RuntimeIdentifiers=linux-x64"
-      "-p:IncludeSourceRevisionInInformationalVersion=false"
       "--framework net9.0"
+      "-p:IncludeSourceRevisionInInformationalVersion=false"
+    ];
+
+    patches = [
+      ./0001-fix-Resolve-fc-match-path-dynamically-on-Linux.patch
     ];
 
     #executables = [ "Beutl" "Beutl.ExceptionHandler" "Beutl.PackageTools.UI" "Beutl.WaitingDialog" ];
