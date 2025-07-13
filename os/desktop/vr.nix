8922e565ba2d1d185b336c097d42c0ab726096a7
turtton <top.gear7509@turtton.net>
2025-07-13 20:20:15 +0900

    ➕ Add wivrn and bs-manager

--- os/desktop/vr.nix
+++ os/desktop/vr.nix
@@ -1,6 +1,18 @@
+{ pkgs, ... }:
 {
-  programs.alvr = {
-    enable = true;
-    openFirewall = true;
+  programs = {
+    alvr = {
+      enable = true;
+      openFirewall = true;
+    };
   };
+  services = {
+    wivrn = {
+      enable = true;
+      openFirewall = true;
+    };
+  };
+  environment.systemPackages = with pkgs; [
+    bs-manager
+  ];
 }
