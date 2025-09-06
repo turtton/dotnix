e539bb726d490c875ccac5cd1e7d20c2c276f47b
turtton <top.gear7509@turtton.net>
2025-09-06 12:49:29 +0900

    🐛 Fix invalid type

--- home-manager/wm/hyprland/waybar/config.nix
+++ home-manager/wm/hyprland/waybar/config.nix
@@ -52,7 +52,7 @@
     "custom/media" = {
       "format" = "  {}";
       "max-lenght" = "40";
-      "interval" = "1";
+      "interval" = 1;
       "exec" = "playerctl metadata --format '{{ artist }} - {{ title }}'";
       "on-click" = "playerctl play-pause";
       "on-click-right" = "playerctl stop";
