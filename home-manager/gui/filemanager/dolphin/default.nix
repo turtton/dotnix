86edf32282136e3e4f09377897bb1be335f26eb1
turtton <top.gear7509@turtton.net>
2025-10-21 01:51:47 +0900

    🔧 Update dolphin settings

--- home-manager/gui/filemanager/dolphin/default.nix
+++ home-manager/gui/filemanager/dolphin/default.nix
@@ -7,9 +7,12 @@
     jetbrains-dolphin-qt6
     kdePackages.ark
   ];
-  xdg.dataFile."dolphin" = {
-    source = ./share;
-    recursive = true;
+  xdg.dataFile = {
+    "dolphin" = {
+      source = ./share;
+      recursive = true;
+    };
+    "state/dolphinstaterc".source = ./dolphinstaterc;
   };
   xdg.mimeApps.defaultApplications = {
     "inode/directory" = [ "${pkgs.dolphin}/share/applications/org.kde.dolphin.desktop" ];
