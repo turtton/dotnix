5a5288d2a0f9c9f72401df1eade7b47003b09972
turtton <top.gear7509@turtton.net>
2026-03-01 22:02:05 +0900

    🩹 Add psycopg slow tests disable patch
    psycopgのプールテスト(test_stats_connect等)がNixサンドボックス内で
    タイミング依存により失敗するため、NixOS/nixpkgs#494547のパッチを追加

--- nixpkgs-patches.nix
+++ nixpkgs-patches.nix
@@ -4,4 +4,9 @@
     url = "https://github.com/NixOS/nixpkgs/pull/494267.patch";
     hash = "sha256-aakMKKwdIIz9FOBFrIVcSPcnlBIo4G+T33NLmFKjsWM=";
   }
+  {
+    meta.description = "python3Packages.psycopg: disable slow tests";
+    url = "https://github.com/NixOS/nixpkgs/pull/494547.patch";
+    hash = "sha256-8KmXEgHpqa3jgEbryTUYQHIW0+AyZJ7FhQW31no17KI=";
+  }
 ]
