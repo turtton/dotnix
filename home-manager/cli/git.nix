cddae0a4fdc7b7db3c9cfc1055a135d252f7dcbb
turtton <top.gear7509@turtton.net>
2026-03-09 11:28:59 +0900

    🔥 Disable github link replacements

--- home-manager/cli/git.nix
+++ home-manager/cli/git.nix
@@ -64,9 +64,9 @@
         alias = {
           log-graph = "log --graph --all --pretty=format:'%Cred%h%Creset -%C(yellow)%d%Creset %s %Cgreen(%cd) %C(bold blue)<%an>%Creset' --abbrev-commit --date=format-local:'%Y/%m/%d %H:%M:%S'";
         };
-        url = {
-          "git@github.com:".insteadOf = "https://github.com/";
-        };
+        # url = {
+        #   "git@github.com:".insteadOf = "https://github.com/";
+        # };
       };
       ignores = [
         ".idea"
