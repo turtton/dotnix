a42dd17f2093b73423222d791b2c7370201a3299
turtton <top.gear7509@turtton.net>
2026-03-30 00:58:44 +0900

    🩹 Apply patch to winboat

--- nixpkgs-patches.nix
+++ nixpkgs-patches.nix
@@ -1,9 +1,9 @@
 [
-  #{
-  #  meta.description = "Update bun to 1.3.10";
-  #  url = "https://github.com/NixOS/nixpkgs/pull/494267.patch";
-  #  hash = "sha256-aakMKKwdIIz9FOBFrIVcSPcnlBIo4G+T33NLmFKjsWM=";
-  #}
+  {
+    meta.description = "winboat: Fix build by specifying dependency versions";
+    url = "https://github.com/NixOS/nixpkgs/pull/503185.patch";
+    hash = "sha256-FSL0o5shDQL5+Om0z40/rPxfglNqPnd6mdeY/hFR3bo=";
+  }
   #{
   #  meta.description = "python3Packages.psycopg: disable slow tests";
   #  url = "https://github.com/NixOS/nixpkgs/pull/494547.patch";
