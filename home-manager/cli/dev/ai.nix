260d7dec9a5d0cf919bb291a6305e718a4c5f8a1
turtton <top.gear7509@turtton.net>
2026-05-19 12:32:57 +0900

    🔧 Add CURSOR_AGENT_PATH

--- home-manager/cli/dev/ai.nix
+++ home-manager/cli/dev/ai.nix
@@ -9,23 +9,28 @@
   imports = [
     ./opencode
   ];
-  home.packages =
-    with pkgs;
-    [
-      codex-latest
-      opencode-latest
-      llm-agents.codex
-      rtk
-      senpi
-      cursor-cli
-    ]
-    ++ pkgs.lib.optionals (!isWsl) [
-      opencode
-    ];
+  home = {
+    packages =
+      with pkgs;
+      [
+        codex-latest
+        opencode-latest
+        llm-agents.codex
+        rtk
+        senpi
+        cursor-cli
+      ]
+      ++ pkgs.lib.optionals (!isWsl) [
+        opencode
+      ];
+    activation.rtk = lib.hm.dag.entryAfter [ "writeBoundary" ] ''
+      ${lib.getExe pkgs.rtk} init -g --opencode
+    '';
+    sessionVariables = {
+      CURSOR_AGENT_PATH = "${lib.getExe pkgs.cursor-cli}";
+    };
+  };
   programs.claude-code = {
     enable = true;
   };
-  home.activation.rtk = lib.hm.dag.entryAfter [ "writeBoundary" ] ''
-    ${lib.getExe pkgs.rtk} init -g --opencode
-  '';
 }
