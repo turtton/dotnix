89a1458e505640b11dca915d28419ec061c75106
turtton <top.gear7509@turtton.net>
2026-05-24 10:21:34 +0900

    🔧 Update monitor settings

--- hosts/maindesk/home-manager-niri.nix
+++ hosts/maindesk/home-manager-niri.nix
@@ -21,29 +21,42 @@
 
   programs.niri.settings = {
     outputs = {
-      "DP-1" = {
+      "ASUSTek COMPUTER INC XG32UCG W2LMTF052146 " = {
         mode = {
-          width = 1920;
-          height = 1080;
-          refresh = 144.0;
+          width = 3840;
+          height = 2160;
+          refresh = 159.975;
         };
         position = {
           x = 0;
           y = 0;
         };
+        scale = 1.2;
+      };
+      "ViewSonic Corporation VX2458-mhd VK0184700653" = {
+        mode = {
+          width = 1920;
+          height = 1080;
+          refresh = 144.001;
+        };
+        position = {
+          x = 0;
+          y = -1080;
+        };
         scale = 1.0;
       };
-      "DP-2" = {
+      "PNP(BNQ) BenQ GL2460 J6G05593SL0" = {
         mode = {
           width = 1920;
           height = 1080;
           refresh = 60.0;
         };
         position = {
-          x = 1920;
-          y = 0;
+          x = -1080;
+          y = -160;
         };
         scale = 1.0;
+        transform.rotation = 270;
       };
       "DP-3" = {
         mode = {
@@ -52,8 +65,8 @@
           refresh = 60.0;
         };
         position = {
-          x = -1920;
-          y = 0;
+          x = -1080 - 1920;
+          y = -100;
         };
         scale = 1.0;
       };
