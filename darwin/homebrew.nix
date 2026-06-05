f9e9e0311992ec28386d04197c44f4dd1dfa9e4b
turtton <top.gear7509@turtton.net>
2026-06-05 22:40:03 +0900

    🚨 Avoid homebrew errors

--- darwin/homebrew.nix
+++ darwin/homebrew.nix
@@ -16,5 +16,9 @@
       "codexbar"
       "background-music"
     ];
+    # https://github.com/nix-darwin/nix-darwin/issues/1787
+    onActivation.extraFlags = [
+      "--force-cleanup"
+    ];
   };
 }
