25b0124cb14a0aa00609cde21a16ef0fff473679
turtton <top.gear7509@turtton.net>
2026-07-01 19:46:44 +0900

    ➕ Add homebrew packages

--- darwin/homebrew.nix
+++ darwin/homebrew.nix
@@ -9,6 +9,7 @@
       "mtgto/macskk"
       "BarutSRB/tap"
       "steipete/tap"
+      "fuwasegu/tap"
     ];
     casks = [
       "macskk"
@@ -16,6 +17,8 @@
       "codexbar"
       "background-music"
       "scroll-reverser"
+      "airlingua"
+      "deskpad"
     ];
     # https://github.com/nix-darwin/nix-darwin/issues/1787
     onActivation.extraFlags = [
