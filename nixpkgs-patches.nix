b35f24ea5880ea42ce244229f80685c4c22a886c
turtton <top.gear7509@turtton.net>
2026-07-03 14:04:19 +0900

    🩹 Disable applied patch

--- nixpkgs-patches.nix
+++ nixpkgs-patches.nix
@@ -1,7 +1,7 @@
 [
-  {
-    meta.description = "Fix darwin compile error";
-    url = "https://github.com/NixOS/nixpkgs/pull/534965.patch";
-    hash = "sha256-qZoXIIY2oNER5af3fr9y2fHZwC6gSFeGTsxGFlpERBk=";
-  }
+  # {
+  #   meta.description = "Fix darwin compile error";
+  #   url = "https://github.com/NixOS/nixpkgs/pull/534965.patch";
+  #   hash = "sha256-qZoXIIY2oNER5af3fr9y2fHZwC6gSFeGTsxGFlpERBk=";
+  # }
 ]
