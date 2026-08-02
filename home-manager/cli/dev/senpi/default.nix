454a9ddc87ebae5ed2b027276ad8ce435c651551
turtton <top.gear7509@turtton.net>
2026-08-03 00:14:55 +0900

    🔧 Switch senpi to full-access preset with pi-sandbox network allowlist
    permissionPresetをworkspaceからfull-accessに変更し、ネットワーク制御は
    pi-sandbox(npm:pi-sandbox@0.6.2)のドメイン許可リストへ移す。
    sandbox.jsonのデプロイと、bwrap/socat/ripgrepの同梱を追加。

--- home-manager/cli/dev/senpi/default.nix
+++ home-manager/cli/dev/senpi/default.nix
@@ -9,9 +9,38 @@ let
 
   settingsJson = pkgs.writeText "senpi-settings.json" (
     builtins.toJSON {
-      permissionPreset = "workspace";
+      permissionPreset = "full-access";
       packages = [
         "${pkgs.omo-senpi}${pkgs.omo-senpi.pluginPath}"
+        "npm:pi-sandbox@0.6.2"
+      ];
+    }
+  );
+
+  sandboxJson = pkgs.writeText "pi-sandbox.json" (
+    builtins.toJSON {
+      enabled = true;
+      network.allowedDomains = [
+        "npmjs.org"
+        "*.npmjs.org"
+        "registry.npmjs.org"
+        "registry.yarnpkg.com"
+        "files.pythonhosted.org"
+        "pypi.org"
+        "*.pypi.org"
+        "github.com"
+        "*.github.com"
+        "api.github.com"
+        "raw.githubusercontent.com"
+        "objects.githubusercontent.com"
+        "crates.io"
+        "*.crates.io"
+        "static.crates.io"
+        "proxy.golang.org"
+        "sum.golang.org"
+        "cache.nixos.org"
+        "rubygems.org"
+        "*.rubygems.org"
       ];
     }
   );
@@ -22,6 +51,9 @@ in
   home.packages = [
     pkgs.senpi
     pkgs.omo-cli
+    pkgs.ripgrep
+    pkgs.bubblewrap
+    pkgs.socat
   ];
 
   home.activation.senpi = lib.hm.dag.entryAfter [ "writeBoundary" ] ''
@@ -30,5 +62,9 @@ in
     [ -f "$senpi_dir/settings.json" ] && $DRY_RUN_CMD mv -f "$senpi_dir/settings.json" "$senpi_dir/settings.json.old"
     $DRY_RUN_CMD cp -f "${settingsJson}" "$senpi_dir/settings.json"
     $DRY_RUN_CMD chmod u+w "$senpi_dir/settings.json"
+
+    [ -f "$senpi_dir/sandbox.json" ] && $DRY_RUN_CMD mv -f "$senpi_dir/sandbox.json" "$senpi_dir/sandbox.json.old"
+    $DRY_RUN_CMD cp -f "${sandboxJson}" "$senpi_dir/sandbox.json"
+    $DRY_RUN_CMD chmod u+w "$senpi_dir/sandbox.json"
   '';
 }
