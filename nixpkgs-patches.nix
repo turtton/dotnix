65e62b8ac9dfce2964b6327e5476b983732ac2a9
turtton <top.gear7509@turtton.net>
2026-09-25 13:05:00 +0900

    🚩 Disable outdated patch

--- nixpkgs-patches.nix
+++ nixpkgs-patches.nix
@@ -1,7 +1,7 @@
 [
-  {
-    meta.description = "Update keybase";
-    url = "https://github.com/NixOS/nixpkgs/pull/497751.patch";
-    hash = "sha256-8Wnn2+/OqNxckHwaYNrxfmb0p0b6vXLm6Keqd7Iyes8=";
-  }
+  # {
+  #   meta.description = "Update keybase";
+  #   url = "https://github.com/NixOS/nixpkgs/pull/497751.patch";
+  #   hash = "sha256-8Wnn2+/OqNxckHwaYNrxfmb0p0b6vXLm6Keqd7Iyes8=";
+  # }
 ]
